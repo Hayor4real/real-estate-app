ab2a7ae8ccb23b14507d39feecafe0e6e34e2dc6
Akindele Ayorinde Emmanuel <73663718+Hayor4real@users.noreply.github.com>
2022-06-15 16:12:23 +0200

    fetch the api

---
+++ utils/fetchApi.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const baseUrl = 'https//bayut.p.rapidapi.com';
+
+export const fetchApi = async (url) => {
+  const { data } = await axios.get(url, {
+    headers: {
+      'X-RapidAPI-Key': '4ce6b6f506mshb923b7c41dd5aecp175edfjsndf5568c2a6e5',
+      'X-RapidAPI-Host': 'bayut.p.rapidapi.com',
+    },
+  });
+};
